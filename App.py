84c668c8d9cfda2dff3087b750e7c5bdeddbb2f4
yoakamlee <yoakamleesoupen@gmail.com>
2025-03-23 17:34:23 +0200

    Setup basic user actions for the dungeon, Search, Move, Leave, these will be used for basic navigation and actions throughout the level (might add more in future) - yls_dev

--- App.py
+++ App.py
@@ -1,3 +1,4 @@
+# Player creation
 player_character = input("Enter your Name: ")
 player_character = player_character.title()
 
@@ -10,16 +11,42 @@ dungeon_enter = input("Enter the dungeon: Yes or No? ")
 dungeon_enter = dungeon_enter.lower().strip()
 
 if dungeon_enter == "yes":
+
     dungeon = True
 
-    while dungeon:
-        print("You have entered the dungeon...")
-        break
+    print(f"{player_character} has entered the dungeon...")
 
+    # dungeon entry
+    while dungeon:
+
+        # Player decision
+        player_action = input("Action- Search, Move, or Leave: ")
+        player_action = player_action.capitalize().strip()
+
+        # Game state
+        match player_action:
+
+            # user actions
+            case "Search":
+                print(f"{player_character} used '{player_action}'")
+                # break
+
+            case "Move":
+                print(f"{player_character} used '{player_action}'")
+                # break
+
+            case "Leave":
+                print(f"{player_character} used '{player_action}'")
+                break
+
+            case _:
+                print("Unknown action !...")
+
+# Exit dungeon
 elif dungeon_enter == "no":
-    print("Cowards...")
+    print("Coward...")
 
 else:
-    breakpoint()
+    print("Unknown action !...")
 
 print("Exited...")
