77fbd7903027e4761bec87b4371adf16671d59d1
yoakamlee <yoakamleesoupen@gmail.com>
2025-03-23 21:35:17 +0200

    Created classes for PlayerStats and Dice, had to move dice into the while loop for proper random generation(TODO: Turn dice into a function), player can now view their player stats. Adjusted the chance for finding common items again. Added shortcut keys for the match-case. - yls_dev

--- App.py
+++ App.py
@@ -5,20 +5,20 @@ player_character = input("Enter your Name: ")
 player_character = player_character.title()
 
 # Player Stats
-player_hp = 10 # Player health
-player_attk = 1 # Player attack power
-player_armor = 0 # Player over health
-player_inventory = []
+class PlayerStats:
+    player_hp = 10 # Player healths
+    player_attk = 1 # Player attack power
+    player_armor = 0 # Player over health
+    player_inventory = []
 
-# All powerful dice object (RN_JESUS)
-dice_d21 = random.randint(1,21)
-dice_d6 = random.randint(1,6)
-dice_d4 = random.randint(1,4)
+
 
 # Search object
-search = dice_d21
 searchable_items_common = ["Ring", "Armor Piece", "Wooden Sword", "Hp Potion"]
 searchable_items_rare = ["Lucky Ring", "Knights armor", "Steel Sword", "Greater Hp Potion"]
+
+# Item object
+
 
 # Dungeon init
 dungeon = False
@@ -36,47 +36,65 @@ if dungeon_enter == "yes":
     # dungeon entry
     while dungeon:
 
+        # All powerful dice object (RN_JESUS)
+        class Dice:
+            dice_d21 = random.randint(1, 21)
+            dice_d6 = random.randint(1, 6)
+            dice_d4 = random.randint(1, 4)
+
         # Player decision
-        player_action = input("Action- Search, Move, Inventory, or Leave: ")
+        player_action = input("Action- Search, Move, Inventory, Stats, or Leave: ")
         player_action = player_action.capitalize().strip()
+
+        search = Dice.dice_d21
 
         # Game state
         match player_action:
 
             # user actions
-            case "Search":
+            case "Search" | "S":
                 print(f"{player_character} used '{player_action}'")
 
                 # Search functions
-                if 6 <= search <= 16:
+                if 5 <= search <= 16:
                     print(f"{player_character} has found an item found")
                     item_searched = random.choice(searchable_items_common)
-                    player_inventory.append(item_searched)
+                    PlayerStats.player_inventory.append(item_searched)
                     print(f"{item_searched} added to {player_character}s' inventory")
 
 
                 elif search > 16:
                     print(f"{player_character} has found a rare item found...")
                     item_searched = random.choice(searchable_items_rare)
-                    player_inventory.append(item_searched)
+                    PlayerStats.player_inventory.append(item_searched)
                     print(f"{item_searched} added to {player_character}s' inventory")
 
                 else:
                     print(f"{player_character} has found nothing...")
 
-            case "Move":
+            case "Move" | "M":
                 print(f"{player_character} used '{player_action}'")
 
-            case "Inventory":
-                if len(player_inventory) == 0:
+            # Check player inventory function
+            case "Inventory" | "I":
+                print(f"{player_character} used '{player_action}'")
+
+                if len(PlayerStats.player_inventory) == 0:
                     print(f"{player_character}s' inventory is empty")
 
                 else:
                     print(f"{player_character}s' inventory")
-                    for item in player_inventory:
+                    for item in PlayerStats.player_inventory:
                         print(item)
 
-            case "Leave":
+            # Check player stats function
+            case "Stats" | "St":
+                print(f"{player_character} used '{player_action}'")
+                print(f"{player_character}s' HP: {PlayerStats.player_hp}")
+                print(f"{player_character}s' ARMOR: {PlayerStats.player_armor}")
+                print(f"{player_character}s' ATTK: {PlayerStats.player_attk}")
+
+            case "Leave" | "L":
                 print(f"{player_character} used '{player_action}'")
                 dungeon = False
 
